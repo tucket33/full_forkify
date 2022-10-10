2260163dee4215a9d6f430e61608faa68328da5b
Danny Asema <brazenstint@tutanota.com>
2022-10-09 21:23:21 -0500

    fixed fraction dependency

--- src/js/views/recipeViews.js
+++ src/js/views/recipeViews.js
@@ -1,5 +1,5 @@
 import View from './View.js';
-import { Fraction } from 'fractional';
+import { numberToFraction} from '../helpers';
 import icons from 'url:../../img/icons.svg';
 //
 class RecipeView extends View {
@@ -119,7 +119,7 @@ class RecipeView extends View {
   <use href="${icons}#icon-check"></use>
 </svg>
 <div class="recipe__quantity">${
-      ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      ing.quantity ? new numberToFraction(ing.quantity).toString() : ''
     }</div>
 <div class="recipe__description">
   <span class="recipe__unit">${ing.unit}</span>
